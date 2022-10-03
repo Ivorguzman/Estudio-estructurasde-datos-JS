40b743c98101dc3ee7d0de02936461166971e1b9
Ivorguzman <66047248+Ivorguzman@users.noreply.github.com>
2022-10-03 17:06:10 -0400

    respaldo 03102022

--- 05_Crear una clase de cola de prioridad.js
+++ 05_Crear una clase de cola de prioridad.js
@@ -1,57 +1,125 @@
 {
-    //! Explicación
+  //! Explicación
 
-    /// Crear una clase de cola de prioridad
+  /// Crear una clase de cola de prioridad
 
-    // En este desafío crearás una cola de prioridad. Una cola de prioridad es un tipo especial de cola en la que los elementos pueden tener información adicional que especifica su prioridad. Esto podría representarse simplemente con un número entero. La prioridad de los elementos anulará el orden de colocación al determinar la secuencia en la que se eliminan los elementos. Si un elemento con mayor prioridad se pone en cola después de los elementos con menor prioridad, el elemento de mayor prioridad se eliminará de la cola antes que todos los demás.
+  // En este desafío crearás una cola de prioridad. Una cola de prioridad es un tipo especial de cola en la que los elementos pueden tener información adicional que especifica su prioridad. Esto podría representarse simplemente con un número entero. La prioridad de los elementos anulará el orden de colocación al determinar la secuencia en la que se eliminan los elementos. Si un elemento con mayor prioridad se pone en cola después de los elementos con menor prioridad, el elemento de mayor prioridad se eliminará de la cola antes que todos los demás.
 
-    // Por ejemplo, imaginemos que tenemos una cola de prioridad con tres elementos:
+  // Por ejemplo, imaginemos que tenemos una cola de prioridad con tres elementos:
 
-    // [['kitten', 2], ['dog', 2], ['rabbit', 2]]
-    
-    // Aquí el segundo valor (un número entero) representa la prioridad del artículo. Si ponemos en cola ['human', 1]con una prioridad de 1(suponiendo que se dé prioridad a las prioridades más bajas), entonces sería el primer elemento que se sacaría de la cola. La colección quedaría así:
+  // [['kitten', 2], ['dog', 2], ['rabbit', 2]]
 
-    // [['human', 1], ['kitten', 2], ['dog', 2], ['rabbit', 2]]
+  // Aquí el segundo valor (un número entero) representa la prioridad del artículo. Si ponemos en cola ['human', 1]con una prioridad de 1(suponiendo que se dé prioridad a las prioridades más bajas), entonces sería el primer elemento que se sacaría de la cola. La colección quedaría así:
 
-}
-
-
-{
-    //! Desafio
-
-    //     Hemos comenzado a escribir un PriorityQueueen el editor de código. Deberá agregar un enqueuemétodo para agregar elementos con prioridad, un dequeuemétodo para eliminar y devolver elementos, un sizemétodo para devolver la cantidad de elementos en la cola, un frontmétodo para devolver el elemento al principio de la cola y, finalmente, un isEmptymétodo que devolverá truesi la cola está vacía o falsesi no lo está.
-
-    // El enqueuedebe aceptar elementos con el formato que se muestra arriba ( ['human', 1]), donde 1representa la prioridad. dequeuey frontdebe devolver solo el nombre del elemento, no su prioridad.
-
-
-
-
-    function PriorityQueue() {
-        this.collection = [];
-        this.printCollection = function () {
-            console.log(this.collection);
-        };
-        // Only change code below this line
-
-        // Only change code above this line
-    }
+  // [['human', 1], ['kitten', 2], ['dog', 2], ['rabbit', 2]]
 
 }
 
 
 {
-    //!Solucion
+  //! Desafio
+
+  //     Hemos comenzado a escribir un PriorityQueueen el editor de código. Deberá agregar un enqueuemétodo para agregar elementos con prioridad, un dequeuemétodo para eliminar y devolver elementos, un sizemétodo para devolver la cantidad de elementos en la cola, un frontmétodo para devolver el elemento al principio de la cola y, finalmente, un isEmptymétodo que devolverá truesi la cola está vacía o falsesi no lo está.
+
+  // El enqueuedebe aceptar elementos con el formato que se muestra arriba ( ['human', 1]), donde 1representa la prioridad. dequeuey frontdebe devolver solo el nombre del elemento, no su prioridad.
 
 
-    function PriorityQueue() {
-        this.collection = [];
-        this.printCollection = function () {
-            console.log(this.collection);
-        };
-        // Only change code below this line
 
-        // Only change code above this line
-    }
+
+  function PriorityQueue() {
+    this.collection = [];
+    this.printCollection = function () {
+      console.log(this.collection);
+    };
+    // Only change code below this line
+
+    // Only change code above this line
+  }
+
+}
+
+
+
+{
+  //!Solucion1
+
+  function PriorityQueue() {
+    this.collection = [];
+
+    this.printCollection = function () {
+      console.log(this.collection);
+    };
+    // Only change code below this line
+
+    this.enqueue = function (item) {
+      console.log(item);
+      console.log(this.collection);
+      // let index = this.collection.findIndex(elem => elem[1] > item[1]);
+      let index = this.collection.findIndex((elem) => {
+        console.log(elem);
+        console.log(item);
+        return elem[1] > item[1];
+      });
+      if (index !== -1) {
+        console.log(index);
+        console.log(item);
+        this.collection.splice(index, 0, item);
+      } else {
+        console.log(index);
+        console.log(item);
+        console.log(this.collection);
+        this.collection.push(item);
+        console.log(this.collection);
+      }
+    };
+
+    this.end = function () {
+      let final = this.collection[this.collection.length - 1][0];
+      return final;
+    };
+
+    this.dequeue = function () {
+      return this.collection.shift()[0];
+    };
+
+    this.size = function () {
+      return this.collection.length;
+    };
+
+    this.isEmpty = function () {
+      return this.size() === 0;
+    };
+
+    this.front = function () {
+      return this.collection[0][0];
+    };
+    // Only change code above this line
+  }
+
+  /*  let pq = new PriorityQueue();
+ 
+   pq.enqueue(['rabbit', 8]);
+   pq.enqueue(['dog', 6])
+   pq.enqueue(['kitten', 12]);
+   pq.enqueue(['human', 1]);
+ 
+ 
+   // console.log(pq.dequeue());
+ 
+ 
+ 
+   console.log("");
+   console.log("");
+   
+   console.log("<<_this.collection_>>")
+   pq.printCollection();
+   console.log(""); 
+   console.log("---------Front------------")
+   console.log(pq.front());
+   console.log("");
+   console.log("--------End-----------")
+   console.log(pq.end());
+  */
 
 }
 
@@ -59,89 +127,174 @@
 
 
 
+// Problema Explicación
+
+// Priority Queue es un tipo de datos abstracto.
+
+// Se puede implementar usando otras estructuras de datos, pero comúnmente se implementa usando un na pila.
+
+// Cada nodo contiene una prioridad. Cuando ponemos en cola un nodo en la cola, se "burbujea" hasta su lugar en la cola.
+
+// En este desafío, necesitamos crear una clase de Priority Queue
+
+// enqueue() - Pone en cola el nuevo nodo en su lugar apropiado en la cola.
+
+// dequeue() - Elimina el primer nodo de la cola.
+
+// front(): este método devuelve el primer nodo que se eliminará de la cola.
+
+// size() - Devuelve el tamaño de la cola.
+
+// isEmpty(): regresa si la cola está vacía.
+
+{
+  ///! Solucion de chat 1 corregida
+
+  function PriorityQueue() {
+    this.collection = [];
+    this.printCollection = function () {
+      console.log(this.collection);
+    };
+
+    this.isEmpty = () => this.collection.length === 0 ? true : false; // verifica si la cola esta vacia
+
+    this.size = () => this.collection.length; // verifica el Tamaño de la cola
+
+    this.enqueue = (arr) => {
+      if (arr[1] === 1) {
+        console.log(arr[1]);
+        console.log(this.collection[1]);
+        this.collection.unshift(arr); // Inserta nuevos elementos al comienzo de una matriz  
+        // console.log(arr[0]);
+        this.collection.sort((a, b) => a[1] - b[1]);
+        console.log(console.log(this.collection));
+        return this.collection;
+      } else {
+        this.collection.push(arr);  // Inserta nuevos elementos al final de una matriz
+        // console.log(arr[0]);
+        this.collection.sort((a, b) => a[1] - b[1]);
+        return this.collection;
+      }
+    };
+    this.dequeue = () => this.collection.shift()[0]; // elimina el primer elemento de la cola
+
+    this.front = () => this.collection[0][0];// muestra el primer elemento de la cola
+  }
 
 
+  /*   let pq = new PriorityQueue();
+  
+    pq.enqueue(['rabbit', 8]);
+    pq.enqueue(['dog', 6]);
+    pq.enqueue(['kitten', 2]);
+    pq.enqueue(['human', 1]);
+  
+  
+    // console.log(pq.dequeue());
+  
+  
+  
+    console.log("");
+    console.log("");
+  
+    console.log("<<_this.collection_>>");
+    pq.printCollection();
+    console.log("");
+    console.log("---------Front------------");
+    console.log(pq.front());
+    console.log("");
+   */
 
-
-
-
-
-
-
-
-
-
+}  
 
 
 {
-
-   function PriorityQueue () {
-  this.collection = [];
-  this.printCollection = function() {
-    console.log(this.collection);
+  ///! Solucion de chat 2
+  class PriorityQueue {
+    constructor() {
+      this.collection = [];
+    };
+    printCollection() {
+      console.log(this.collection);
+    };
+    enqueue(value) {
+      if (typeof value === "object" && value.length === 2 && typeof value[1] === "number") {
+        let index = 0;
+        while (this.collection[index] && this.collection[index][1] < value[1] + 1) {
+          index++;
+        }
+        this.collection.splice(index, 0, value);
+      }
+    };
+    dequeue() {
+      if (this.collection.length > 0) return this.collection.pop()[0];
+    };
+    front() {
+      return this.collection[this.collection.length - 1][0];
+    };
+    size() {
+      return this.collection.length;
+    };
+    isEmpty() {
+      return this.collection.length === 0;
+    };
   };
-  // Only change code below this line
-  this.enqueue = function(item) {
-    let index = this.collection.findIndex(elem => elem[1] > item[1]);
-    if (index !== -1) {
-      this.collection.splice(index, 0, item);
-    } else {
-      this.collection.push(item);
-    }
-  }
+  let pq = new PriorityQueue();
 
-  this.dequeue = function() {
-    return this.collection.shift()[0];
-  }
+  pq.enqueue(['rabbit', 8]);
+  pq.enqueue(['dog', 6]);
+  pq.enqueue(['kitten', 2]);
+  pq.enqueue(['human', 1]);
 
-  this.size = function() {
-    return this.collection.length;
-  }
 
-  this.isEmpty = function() {
-    return this.size() === 0;
-  }
+  // console.log(pq.dequeue());
 
-  this.front = function() {
-    return this.collection[0][0];
-  }
-  // Only change code above this line
+
+
+  console.log("");
+  console.log("");
+
+  console.log("<<_this.collection_>>");
+  pq.printCollection();
+  console.log("");
+  console.log("---------Front------------");
+  console.log(pq.front());
+  console.log("");
+
+  ///Muchas gracias por su ayuda... Al menos encontré el problema. De la matriz hay que eliminar el primer elemento, no el último. Cambié el método pop() y ahora paso la prueba Gracias.;
+
+
 }
 
-    let colaPrioridad = new PriorityQueue();
-
-    console.log(colaPrioridad.printCollection());
-    console.log(colaPrioridad.enqueue(['kitten', 1, 'dog', , 'rabbit', 3]));
-    console.log(colaPrioridad.push(['kitten', 1, 'dog', 2, 'rabbit', 3]));
-
-
-    // Problema Explicación
-    // Priority Queue es un tipo de datos abstracto.
-    // Se puede implementar usando otras estructuras de datos, pero comúnmente se implementa usando un na pila.
-    // Cada nodo contiene una prioridad. Cuando ponemos en cola un nodo en la cola, se "burbujea" hasta su lugar en la cola.
-    // En este desafío, necesitamos crear una clase de Priority Queue basada en Min-Heap con algunos métodos:
-    // enqueue() - Pone en cola el nuevo nodo en su lugar apropiado en la cola.
-    // dequeue() - Elimina el primer nodo de la cola.
-    // front(): este método devuelve el primer nodo que se eliminará de la cola.
-    // size() - Devuelve el tamaño de la cola.
-    // isEmpty(): regresa si la cola está vacía.
-    // SD	Acceso	Búsqueda	Insertar	Borrar
-    // cola de prioridad	1	1	iniciar sesión	iniciar sesión
-    // Enlaces relevantes
-    // Wikipedia 121
-
-}
 
 {
-    //  !   Pruebas
+  /// Ejemplo de sort con objetos
+  const items = [
+    { name: 'Edward', value: 21 },
+    { name: 'Sharpe', value: 37 },
+    { name: 'And', value: 45 },
+    { name: 'The', value: -12 },
+    { name: 'Magnetic', value: 13 },
+    { name: 'Zeros', value: 37 }
+  ];
 
-    // esperando :Su PriorityQueueclase debe tener un enqueuemétodo.
-    // esperando :Su PriorityQueueclase debe tener un dequeuemétodo.
-    // esperando :Su PriorityQueueclase debe tener un sizemétodo.
-    // esperando :Su PriorityQueueclase debe tener un frontmétodo.
-    // esperando :Su PriorityQueueclase debe tener un isEmptymétodo.
-    // esperando :Su PriorityQueueclase debe realizar un seguimiento correcto de la cantidad actual de elementos utilizando el sizemétodo a medida que los elementos se ponen en cola y se quitan de la cola.
-    // esperando :El frontmétodo debe devolver el elemento correcto al principio de la cola a medida que los elementos se ponen en cola y se quitan de la cola.
-    // esperando :El isEmptymétodo debería regresar truecuando la cola esté vacía.
-    // esperando :La cola de prioridad debe devolver los artículos con una prioridad más alta antes que los artículos con una prioridad más baja y, de lo contrario, devolver los artículos en el orden primero en entrar, primero en salir.
+  // sort by value
+  console.log(items);
+  console.log(items.sort((a, b) => a.value - b.value));
+}
+
+
+{
+  let x = [2];
+  x[0] = 1;
+  x[1] = 2;
+  console.log(x); 
+
+  let y = new Array(2);
+  y[0] = "A";
+  y[1] = "B";
+
+
+  console.log(y);
+  console.log(y[0][1]);
 }
--- 06_Crear una cola circular.js
+++ 06_Crear una cola circular.js
@@ -9,11 +9,11 @@
     // [null, null, null, null, null]
     //  ^Read @ 0
     //  ^Write @ 0
-    // Aquí, la lectura y la escritura están en la posición 0. Ahora la cola obtiene 3 nuevos registros a, by c. Nuestra cola ahora se ve así:
+    // Aquí, la lectura y la escritura están en la posición 0. Ahora la cola obtiene 3 nuevos registros a, b y c. Nuestra cola ahora se ve así:
 
     // [a, b, c, null, null]
     //  ^Read @ 0
-    //           ^Write @ 3
+    //   ^Write @ 3
     // A medida que lee el cabezal de lectura, puede eliminar valores o mantenerlos:
 
     // [null, null, null, null, null]
@@ -123,7 +123,7 @@
     //     Problema Explicación
     // En este desafío creamos una estructura de datos de cola circular.
 
-    // Primero, necesitamos crear una matriz sizecon todos los elementos establecidos en null.
+    // Primero, necesitamos crear una matriz size con todos los elementos establecidos en null.
 
     // Luego creamos un método equeue, que mueve el puntero de escritura pero no excede el puntero de lectura.
 
@@ -197,7 +197,7 @@
     {
 
 
-        //  Solución 1 (haga clic para mostrar/ocultar)
+        //  Solución 2 (haga clic para mostrar/ocultar)
 
 
         class CircularQueue {
@@ -240,3 +240,134 @@
     }
 
 }
+
+
+{
+    //soluconn del chat
+
+    class CircularQueue {
+        constructor(size) {
+
+            this.queue = [];
+            this.read = 0;
+            this.write = 0;
+            this.max = size - 1;
+            this.count = 0;
+
+            while (size > 0) {
+                this.queue.push(null);
+                size--;
+            }
+        }
+        checkEmpty() {
+            var count = 0;
+            for (let i = 0; i <= this.max; i++) {
+                if (this.queue[i] == null) {
+                    count++;
+                }
+            }
+            if (count == this.max + 1) {
+                return true;
+            }
+            return false;
+        }
+        checkFull() {
+            var count = 0;
+            for (let i = 0; i <= this.max; i++) {
+                if (this.queue[i] == null) {
+                    count++;
+                }
+            }
+            if (count > 0) {
+                return false;
+            }
+            return true;
+        }
+
+        print() {
+            return this.queue;
+        }
+
+        enqueue(item) {
+            // Only change code below this line
+            if (!this.checkFull()) {
+                this.queue[this.write] = item;
+                this.write = (this.write + 1) % (this.max + 1);
+            }
+        }
+        // Only change code above this line
+
+        dequeue() {
+            // Only change code below this line
+            if (!this.checkEmpty()) {
+                let val = this.queue[this.read];
+                this.queue[this.read] = null;
+                this.read = (this.read + 1) % (this.max + 1);
+                return val;
+            }
+            else return null;
+            // Only change code above this line
+        }
+    }
+}
+
+/* 
+
+hbar1st
+junio '18
+No usaría el método checkEmpty en absoluto. Para la lectura solo tienes que preocuparte del registro actual. Comprobar que toda la cola está vacía no parece correcto. Es decir, creo que verificaría si hay algo para leer en el punto actual. Si es así, devuélvelo, si no, devuelve nulo. Ignore todos los demás puntos de la cola.
+
+
+
+
+olaruandrei
+junio '18
+reemplazó /// "if(!this.checkEmpty())" con "if(this.queue[this.read])" y nada cambió:ceñudo:
+
+
+
+
+hbar1st
+junio '18
+
+dequeue() {
+/// Solo cambie el código debajo de esta línea
+let val=this.queue[this.read];
+if (val) {
+/// this.queue[this.read]=null; //no necesito esto, ya que esta función es solo para leer
+this.read=(this.read+1)%(this.max+1); //cambiar el contador de lectura
+return val;
+}
+más devuelve nulo;
+/// Solo cambia el código arriba de esta línea
+}
+
+Entonces, si el anterior es su nuevo método, luego de mirarlo, diría que aún debe preocuparse por la posición del contador de escritura. Si el contador de escritura está, por ejemplo, en la posición 5 y está intentando leer la posición 5 o superior, entonces debe devolver nulo.
+
+Pensemos en ello en ejemplos más concretos:
+
+Alguien escribe (pone en cola) 5 registros A, B, C, D, E
+y el tamaño máximo de la cola es 6, por lo que mi contador de escritura apunta a la sexta posición
+. Luego trato de leer en la posición 1, 2, 3, 4, 5 y luego intente leer 6, entonces debería obtener un nulo.
+Otro ej. ¿Qué pasa si alguien pone en cola 6 registros A, B, C, D, E, F y luego intenta leer 7?
+Entonces, eso significaría que mi contador de escritura después de escribir 6 apunta a la A nuevamente, mientras que mi contador de lectura después de 6 lecturas apunta a la A (listo para leer A). Su código debe decir 'deténgase allí mismo' porque 'A' también es donde apunta el contador de escritura, por lo tanto, no debería volver a leer A.
+
+Al menos, así es como interpreto el criterio de éxito "Intentando desencolar más allá del puntero de escritura...", así que espero que esto ayude.
+
+
+
+
+olaruandrei
+junio '18
+entonces, por lo que dices, no puedo tener en algún momento una cola vacía como queue.length=4 =[null, null, null, null]. Porque eso significa que entendí mal el problema. ok, intentare solucionar este problema y te aviso
+
+
+
+
+hbar1st
+junio '18
+No del todo, pero estaba tratando de decir que deberías comprobar la posición de los punteros de escritura frente a los de lectura. En ningún momento debe intentar leer algo que aún no ha escrito (y eso no depende de nulo, sino que solo depende de la posición actual de los punteros). Puede leer A, B, C, D, E, F solo una vez. No debe volver a leer A, B, etc. solo porque alguien llama a la cola. Solo léalos si el contador de escritura está por delante de la lectura.
+
+
+
+*/
